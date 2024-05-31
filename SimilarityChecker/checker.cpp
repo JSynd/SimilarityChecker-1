f1582e4fbb7a5b6f46ee9b9a938f4300cd6f11c4
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-31 09:26:57 +0900

    [refactor] refactor length check (same length)

--- SimilarityChecker/checker.cpp
+++ SimilarityChecker/checker.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 class SimilarityChecker {
 public:
-
-	int checkLength(string& str1, string& str2) {
-		return 60;
+	void setTargetStrings(string& str1, string& str2) {
+		if (str1.size() >= str2.size()) {
+			str_1 = str1;
+			str_2 = str2;
+		}
+		else {
+			str_1 = str2;
+			str_2 = str1;
+		}
 	}
+	int checkLength() {
+		if (str_1.size() == str_2.size()) return 60;
+		return 0;
+	}
+private:
+	string str_1, str_2;
 };
--- SimilarityChecker_test/test.cpp
+++ SimilarityChecker_test/test.cpp
@@ -1,8 +1,17 @@
 #include "pch.h"
 #include "../SimilarityChecker/checker.cpp"
 
-TEST(SimilarityChecker, LengthScore60) {
+class SimilarityCheckerFixture : public testing::Test {
+public:
 	SimilarityChecker checker;
-	int score = checker.checkLength(string("ABC"), string("DEF"));
+
+	int getLengthSimilarityScore(string str1, string str2) {
+		checker.setTargetStrings(str1, str2);
+		return checker.checkLength();
+	}
+};
+
+TEST_F(SimilarityCheckerFixture, LengthScore60) {
+	int score = getLengthSimilarityScore("ABC", "DEF");
 	EXPECT_EQ(score, 60);
 }
