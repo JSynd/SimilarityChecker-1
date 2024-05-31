91f33944abe6da9a1e2193de0fc69b02473be530
hyeokseo.joo <hyeokseo.joo@samsung.com>
2024-05-31 10:14:47 +0900

    [feat] add unit test for alpha check ( excpetion - lower case )

--- SimilarityChecker/checker.cpp
+++ SimilarityChecker/checker.cpp
@@ -1,4 +1,11 @@
+#include <string>
+#include <stdexcept>
+
+using namespace std;
+
 class SimilarityChecker {
 public:
-
+	void checkAlpha(string& str1, string& str2) {
+		throw invalid_argument("error!");
+	}
 };
--- SimilarityChecker_test/test.cpp
+++ SimilarityChecker_test/test.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
+#include "../SimilarityChecker/checker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(SimilarityCheck, AlphaScore_UpperCaseOnly) {
+	SimilarityChecker checker;
+	EXPECT_THROW(checker.checkAlpha(string("abc"), string("edf")), invalid_argument);
+	EXPECT_THROW(checker.checkAlpha(string("ABC"), string("EDf")), invalid_argument);
+	EXPECT_THROW(checker.checkAlpha(string("ABc"), string("EDF")), invalid_argument);
 }
